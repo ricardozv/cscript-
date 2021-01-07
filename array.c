ed716454e28fa59ef33ce6f9697633f3c54b742e
ricardozv <ricardozv@gmail.com>
2021-01-07 16:30:19 -0300

    up code

---
+++ array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+    char palavrasecreta[20];
+
+    palavrasecreta[0] = 'M';
+    palavrasecreta[1] = 'E';
+    palavrasecreta[2] = 'L';
+    palavrasecreta[3] = 'A';
+    palavrasecreta[4] = 'N';
+    palavrasecreta[5] = 'C';
+    palavrasecreta[6] = 'I';
+    palavrasecreta[7] = 'A';
+    palavrasecreta[8] = 'M';
+
+    printf("%c%c%c%c%c%c%c%c\n",
+    palavrasecreta[0],palavrasecreta[1], palavrasecreta[2], palavrasecreta[3],
+    palavrasecreta[4], palavrasecreta[5], palavrasecreta[6], palavrasecreta[7]);
+
+
+
+}
---
+++ arrayChar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main () {
+    char palavrasecreta[20];
+    sprintf(palavrasecreta, "MELANCIA");
+    printf(palavrasecreta, "%s");
+}
