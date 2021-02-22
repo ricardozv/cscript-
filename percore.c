6a3bf64286c2fc3cca51b0ff9b1b3f8c2537a79a
ricardozv <ricardosilvanet03@gmail.com>
2021-02-22 16:09:41 -0300

    upload

---
+++ percore.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int notas[5];
+    notas[0] = 1;
+    notas[1] = 4;
+    notas[2] = 7;
+    notas[3] = 5;
+    notas[4] = 10;
+
+ for (int i = 0; i < 5; i++) {
+    printf(" %d \n", notas[i]);
+        }
+
+}
--- teste.c
+++ teste.c
@@ -1,1 +1,25 @@
-# Commit test
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+    char palavrasecreta[20];
+
+    sprintf(palavrasecreta, "MELANCIA");
+
+    int acertou = 0;
+    int enforcou = 1;
+
+    do {
+
+        for(int i = 0; i < strlen(palavrasecreta); i++) {
+            printf("_ ");
+        }
+        printf("\n");
+
+        char chute;
+        scanf("%c", &chute);
+
+    } while(!acertou && !enforcou);
+
+
+}
