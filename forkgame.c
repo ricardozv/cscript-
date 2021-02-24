674c8df7dbba56ad7a0015eab5aa2fd56ae54587
ricardozv <ricardosilvanet03@gmail.com>
2021-02-24 15:00:29 -0300

    upload

--- forkgame.c
+++ forkgame.c
@@ -1,14 +1,72 @@
 #include <stdio.h>
+#include <string.h>
+
+char palavrasecreta[20];
+char chutes[26];
+int tentativas = 0;
+
+void abertura() {
+    printf("/****************/\n");
+    printf("/ Jogo de Forca */\n");
+    printf("/****************/\n\n");
+}
+
+void chuta() {
+    char chute;
+    printf("Qual letra? ");
+    scanf(" %c", &chute);
+
+    chutes[tentativas] = chute;
+}
+
+int jachutou(char letra) {
+    int achou = 0;
+    for(int j = 0; j < tentativas; j++) {
+        if(chutes[j] == letra) {
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
+void desenhaforca() {
+
+    printf("Você já deu %d chutes\n", tentativas);
+
+    for(int i = 0; i < strlen(palavrasecreta); i++) {
+
+        if(jachutou(palavrasecreta[i])) {
+            printf("%c ", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
+
+    }
+    printf("\n");
+
+}
+
+void escolhepalavra() {
+    sprintf(palavrasecreta, "MELANCIA");
+}
 
 int main() {
-    char letra1 ='M';
-    char letra2 ='E';
-    char letra3 ='L';
-    char letra4 ='A';
-    char letra5 ='N';
-    char letra6 ='C';
-    char letra7 ='I';
-    char letra8 ='A';
 
-    printf("%c%c%c%c%c%c%c%c\n", letra1, letra2, letra3, letra4, letra5, letra6, letra7, letra8);
+    int acertou = 0;
+    int enforcou = 0;
+
+    abertura();
+    escolhepalavra();
+
+    do {
+
+        desenhaforca();
+        chuta();
+
+        tentativas++;
+
+    } while (!acertou && !enforcou);
+
 }
