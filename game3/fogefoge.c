ec85bc6b1c2bb1a1e3d38e38ccff7b983b74a40e
ricardozv <ricardosilvanet03@gmail.com>
2021-03-20 01:28:16 -0300

    up code

---
+++ game3/fogefoge.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fogefoge.h"
+#include "mapa.h"
+
+MAPA m;
+POSICAO heroi;
+
+
+int acabou() {
+	return 0;
+}
+
+void move(char direcao) {
+
+	m.matriz[heroi.x][heroi.y] = '.';
+
+	switch(direcao) {
+		case 'a':
+			m.matriz[heroi.x][heroi.y-1] = '@';
+			heroi.y--;
+			break;
+		case 'w':
+			m.matriz[heroi.x-1][heroi.y] = '@';
+			heroi.x--;
+			break;
+		case 's':
+			m.matriz[heroi.x+1][heroi.y] = '@';
+			heroi.x++;
+			break;
+		case 'd':
+			m.matriz[heroi.x][heroi.y+1] = '@';
+			heroi.y++;
+			break;
+	}
+	
+}
+
+int main() {
+	
+	lemapa(&m);
+	encontramapa(&m, &heroi, '@');
+
+	do {
+		imprimemapa(&m);
+
+		char comando;
+		scanf(" %c", &comando);
+
+		move(comando);
+
+	} while (!acabou());
+
+	liberamapa(&m);
+}
